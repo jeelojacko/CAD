a085b353065ae9220dfc69510ac896e7e0cada06
jellojacko <159662187+jeelojacko@users.noreply.github.com>
2025-06-06 08:14:01 -0300

    Remove example add function

--- survey_cad/src/lib.rs
+++ survey_cad/src/lib.rs
@@ -11,20 +11,3 @@ pub mod pmetra;
 pub mod render;
 pub mod surveying;
 pub mod truck_integration;
-
-/// Adds two numbers together. Example function.
-#[allow(dead_code)]
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
