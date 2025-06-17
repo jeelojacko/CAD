233263f209a33b622b4583b08f8151602f619117
jellojacko <159662187+jeelojacko@users.noreply.github.com>
2025-06-17 11:55:10 -0300

    Add files via upload

---
+++ truck-master/truck-shapeops/src/lib.rs
@@ -0,0 +1,21 @@
+//! Crate for operation shapes. Provides boolean operations to Solid, and shape healing for importing shapes from other CAD systems.
+
+#![cfg_attr(not(debug_assertions), deny(warnings))]
+#![deny(clippy::all, rust_2018_idioms)]
+#![warn(
+    missing_docs,
+    missing_debug_implementations,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_import_braces,
+    unused_qualifications
+)]
+
+mod healing;
+pub use healing::{RobustSplitClosedEdgesAndFaces, SplitClosedEdgesAndFaces};
+mod transversal;
+pub use transversal::{and, or, ShapeOpsCurve, ShapeOpsSurface};
+mod alternative;
+mod fillet;
